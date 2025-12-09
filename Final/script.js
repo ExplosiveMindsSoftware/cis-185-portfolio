76c2d3227a9747cd037e0b2e4d0fbe5e995663ea
ExplosiveMindsSoftware <jorge.cordero.jr@gmail.com>
2025-12-09 14:14:35 -0800

    Added player sprites, started writing to README.md, fix a couple bugs, and got started on running and jumping mechanics for the player

--- Final/script.js
+++ Final/script.js
@@ -2,31 +2,97 @@ window.addEventListener ('load', function () {
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
 
+    // Set canvas dimensions
     canvas.width=800;
-    canvas.width=720;
+    canvas.height=720;
 
+    //Class to handle player input
     class InputHandler {
         constructor(){
             this.keys = [];
             window.addEventListener('keydown', e => {
                 console.log(e.key);
-                if(e.key === 'ArrowDown' && this.keys.indexOf(e.key) === -1){
+                if((e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight')
+                    && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key);
                 }
                 console.log(e.keys, this.keys);
             });
 
             window.addEventListener('keyup', e => {
-                if(e.key === 'ArrowDown'){
+                if( e.key === 'ArrowDown' ||
+                    e.key === 'ArrowUp' ||
+                    e.key === 'ArrowLeft' ||
+                    e.key === 'ArrowRight'){
                     this.keys.splice(this.keys.indexOf(e.key), 1);
                 }
-            console.log(e.keys, this.keys);
             });
         }
     }
 
     class Player {
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 50;
+            this.height = 65;
+            this.x = 0;
+            this.y = this.gameHeight - this.height;
+            this.image = document.getElementById('LuffyPlayer');
+            this.frameX = 0;
+            this.frameY = 0;
+            this.speed = 0;
+            this.vy = 0;
+            this.weight = 1;
+        }
+        draw(context){
+            context.fillStyle = 'black';
+            context.fillRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
+        }
+        update(input){
+            this.x += this.speed;
+            if(input.keys.indexOf('ArrowRight') > -1){
+                this.speed = 5;
+            }
 
+            else if(input.keys.indexOf('ArrowLeft') > -1){
+                this.speed = -5;
+            }
+
+            else if(input.keys.indexOf('ArrowUp') > -1 && this.onGround()){
+                this.vy -= 30;
+            }
+
+            else{
+                this.speed = 0;
+            }
+
+            //Horizontal movement
+            this.x += this.speed;
+            if(this.x < 0) this.x = 0;
+            else if(this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width;
+            
+            //Vertical movement
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+                this.frameY = 1;
+            }
+
+            else {
+                this.vy = 0;
+            }
+
+            if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height;
+        }
+
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
+        }
     }
 
     class Background {
@@ -46,4 +112,16 @@ window.addEventListener ('load', function () {
     }
 
     const input = new InputHandler();
+    const player = new Player(canvas.width, canvas.height);
+
+    function animate(){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        player.update(input);
+
+        player.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
 });
